f48f7a1c8329261b5fd4bab20a10c8b94b6b58d1
John Paxton <johnpaxton@gmail.com>
2026-03-31 12:04:59 -0400

    Custom resolvers

--- labs/solutions/lab-resolvers.js
+++ labs/solutions/lab-resolvers.js
@@ -1,4 +1,4 @@
-import allData from '../data/all-data.json' with { type: 'json' };
+import allData from '../../data/all-data.json' with { type: 'json' };
 
 let {
 	// eslint-disable-next-line no-unused-vars
@@ -19,15 +19,31 @@ export const resolvers = {
 			return users;
 		},
 
-		/* 		
 		payments() {
 			return payments;
 		},
- 		*/
-		/*
-		users() {
-			return users;
+	},
+
+	Payment: {
+		// Overrides an existing default resolver
+		visibility(parent) {
+			if (parent.visibility && typeof parent.visibility === 'string') {
+				return parent.visibility.toUpperCase();
+			} else {
+				return null;
+			}
 		},
-		*/
+
+		// ADDS a resolver for a field that doesn't exist in the underlying data
+		localeAmount(parent) {
+			let formatter = Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
+
+			return `${formatter.format(parent.amount)}`;
+		},
+
+		senderDetails(parent) {
+			let senderId = parent.sender;
+			return users.find((user) => user.userId === senderId);
+		},
 	},
 };
